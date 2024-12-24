e9c2839f396cc7f8bc7108272da568400684f2da
Nguyen Tran Gia Kiet <giakietnt2205@gmail.com>
2024-12-24 15:00:21 +0700

    update db

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -1,10 +1,32 @@
+using Restaurants.API.Controllers;
+using Restaurants.Infrastructure.Extensions;
+using Restaurants.Infrastructure.Seeders;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddScoped<IWeatherForecastService, WeatherForecastService>();
+
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantSeeder>();
+
+await seeder.Seed();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 // Configure the HTTP request pipeline.
 
---
+++ Restaurants.Domain/Repositories/IRestaurantsRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Domain.Repositories
+{
+    public interface IRestaurantsRepository
+    {
+        Task<IEnumerable<Restaurant>> GetAllAsync();
+    }
+}
